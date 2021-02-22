054f65951b869de7d09d3862f326f90fc7aa1e78
Mathieu Méa <mathieu.mea@gmail.com>
2021-02-22 10:50:03 -0500

    Add Ottawa OC Transpo provider commons

---
+++ src/main/java/org/mtransit/commons/provider/OttawaOCTranspoProviderCommons.kt
@@ -0,0 +1,25 @@
+package org.mtransit.commons.provider
+
+import org.mtransit.commons.CleanUtils
+import org.mtransit.commons.StringUtils.EMPTY
+import java.util.regex.Pattern
+
+object OttawaOCTranspoProviderCommons {
+
+    private val FIX_CAIRINE_WILSON_ = CleanUtils.cleanWords("carine wilson")
+    private val FIX_CAIRINE_WILSON_REPLACEMENT = CleanUtils.cleanWordsReplacement("Cairine Wilson")
+
+    private val REMOVE_SECOND_LANGUAGE = Pattern.compile("( ~ .*$)") // FIXME i18n head-signs
+
+    @JvmStatic
+    fun cleanTripHeadsign(tripHeadSign: String, @Suppress("UNUSED_PARAMETER") vararg ignoreWords: String): String {
+        var newTripHeadSign = tripHeadSign
+        newTripHeadSign = REMOVE_SECOND_LANGUAGE.matcher(newTripHeadSign).replaceAll(EMPTY)
+        newTripHeadSign = FIX_CAIRINE_WILSON_.matcher(newTripHeadSign).replaceAll(FIX_CAIRINE_WILSON_REPLACEMENT)
+        newTripHeadSign = CleanUtils.fixMcXCase(newTripHeadSign)
+        newTripHeadSign = CleanUtils.cleanBounds(newTripHeadSign)
+        newTripHeadSign = CleanUtils.cleanSlashes(newTripHeadSign)
+        newTripHeadSign = CleanUtils.cleanLabel(newTripHeadSign)
+        return newTripHeadSign
+    }
+}
