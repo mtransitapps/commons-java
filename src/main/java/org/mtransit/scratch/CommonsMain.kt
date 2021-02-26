5378785d6fb2846be3ad80da00f5a0e0b559ff8b
Mathieu Méa <mathieu.mea@gmail.com>
2021-02-26 17:11:07 -0500

    Add Main scratch class to quickly run code

---
+++ src/main/java/org/mtransit/scratch/CommonsMain.kt
@@ -0,0 +1,11 @@
+package org.mtransit.scratch
+
+object CommonsMain {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        // QUICKLY RUN CODE
+        val result = "result"
+        println("result: '$result'.")
+    }
+}
