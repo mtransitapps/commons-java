6be80eda0ed694e109181f316782c211b101ce0f
Mathieu Méa <mathieu.mea@gmail.com>
2021-03-17 11:03:46 -0400

    Feature flag: cache data-sources

--- src/main/java/org/mtransit/commons/FeatureFlags.kt
+++ src/main/java/org/mtransit/commons/FeatureFlags.kt
@@ -1,7 +1,11 @@
 package org.mtransit.commons
 
+@Suppress("unused")
 object FeatureFlags {
 
     const val F_PRE_FILLED_DB = false
     // const val F_PRE_FILLED_DB = true // WIP
+
+    const val F_CACHE_DATA_SOURCES = false
+    // const val F_CACHE_DATA_SOURCES = true // WIP
 }
