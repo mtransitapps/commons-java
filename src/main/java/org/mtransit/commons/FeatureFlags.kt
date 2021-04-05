682b36b481a462b4e7fcf13c1b2e264a0ab11961
Mathieu Méa <mathieu.mea@gmail.com>
2021-04-05 07:53:40 -0400

    DataSourcesCache: turn ON feature.

--- src/main/java/org/mtransit/commons/FeatureFlags.kt
+++ src/main/java/org/mtransit/commons/FeatureFlags.kt
@@ -6,6 +6,4 @@ object FeatureFlags {
     const val F_PRE_FILLED_DB = false
     // const val F_PRE_FILLED_DB = true // WIP
 
-    const val F_CACHE_DATA_SOURCES = false
-    // const val F_CACHE_DATA_SOURCES = true // WIP
 }
