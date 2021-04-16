4764a322464e501699562bcfd12a040857ec1f07
Mathieu Méa <mathieu.mea@gmail.com>
2021-04-16 13:26:53 -0400

    AppUpdate: feature flag disabled again (WIP)
    This reverts commit 5a31877557659e1833e9b899d3223500dead36d4.

--- src/main/java/org/mtransit/commons/FeatureFlags.kt
+++ src/main/java/org/mtransit/commons/FeatureFlags.kt
@@ -6,7 +6,7 @@ object FeatureFlags {
     const val F_PRE_FILLED_DB = false
     // const val F_PRE_FILLED_DB = true // WIP
 
-    // const val F_APP_UPDATE = false
-    const val F_APP_UPDATE = true // WIP
+    const val F_APP_UPDATE = false
+    // const val F_APP_UPDATE = true // WIP
 
 }
