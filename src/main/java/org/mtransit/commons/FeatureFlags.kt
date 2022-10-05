9c2303c5a3bfc27eae4881810df03a7b210098c1
Mathieu Méa <mathieu.mea@gmail.com>
2022-10-05 10:44:41 -0400

    Feature flags: enable app update, ALL CAPS off, direction img by default

--- src/main/java/org/mtransit/commons/FeatureFlags.kt
+++ src/main/java/org/mtransit/commons/FeatureFlags.kt
@@ -6,18 +6,9 @@ object FeatureFlags {
     const val F_PRE_FILLED_DB = false
     // const val F_PRE_FILLED_DB = true // WIP
 
-    // const val F_APP_UPDATE = false
-    const val F_APP_UPDATE = true // WIP
-
     const val F_TRANSITION = false
     // const val F_TRANSITION = true // WIP
 
     const val F_NAVIGATION = false
     // const val F_NAVIGATION = true // WIP
-
-    const val F_ALL_CAPS_OFF = false
-    // const val F_ALL_CAPS_OFF = true // WIP
-
-    const val F_DIRECTION_IMG = false
-    // const val F_DIRECTION_IMG = true // WIP
 }
