ee880818e46e0242f03ae8cff5dd556e3a2096d0
Mathieu Méa <mathieu.mea@gmail.com>
2022-11-15 10:01:27 -0500

    Feature flags: non-UI flag depends on UI flag

--- src/main/java/org/mtransit/commons/FeatureFlags.kt
+++ src/main/java/org/mtransit/commons/FeatureFlags.kt
@@ -1,6 +1,6 @@
 package org.mtransit.commons
 
-@Suppress("unused")
+@Suppress("unused", "SimplifyBooleanWithConstants")
 object FeatureFlags {
 
     const val F_PRE_FILLED_DB = false
@@ -12,13 +12,13 @@ object FeatureFlags {
     const val F_NAVIGATION = false
     // const val F_NAVIGATION = true // WIP
 
-    const val F_SCHEDULE_DESCENT_ONLY = false
-    // const val F_SCHEDULE_DESCENT_ONLY = true // WIP
     const val F_SCHEDULE_DESCENT_ONLY_UI = false
     // const val F_SCHEDULE_DESCENT_ONLY_UI = true // WIP
+    const val F_SCHEDULE_DESCENT_ONLY = F_SCHEDULE_DESCENT_ONLY_UI && false
+    // const val F_SCHEDULE_DESCENT_ONLY = F_SCHEDULE_DESCENT_ONLY_UI && true // WIP
 
-    const val F_HTML_POI_NAME = false
-    // const val F_HTML_POI_NAME = true // WIP
     const val F_HTML_POI_NAME_UI = false
     // const val F_HTML_POI_NAME_UI = true // WIP
+    const val F_HTML_POI_NAME = F_HTML_POI_NAME_UI && false
+    // const val F_HTML_POI_NAME = F_HTML_POI_NAME_UI && true // WIP
 }
