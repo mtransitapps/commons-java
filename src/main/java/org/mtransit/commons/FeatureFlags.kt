d15ac348151aa412b93c6a2382cd91c52e561c4f
Mathieu Méa <mathieu.mea@gmail.com>
2023-01-09 11:36:41 -0500

    Feature flags > cleanup

--- src/main/java/org/mtransit/commons/FeatureFlags.kt
+++ src/main/java/org/mtransit/commons/FeatureFlags.kt
@@ -13,16 +13,6 @@ object FeatureFlags {
     const val F_NAVIGATION = false
     // const val F_NAVIGATION = true // WIP
 
-    // const val F_SCHEDULE_DESCENT_ONLY_UI = false
-    const val F_SCHEDULE_DESCENT_ONLY_UI = true // WIP // TODO delete when :parser cleanup
-    // const val F_SCHEDULE_DESCENT_ONLY = F_SCHEDULE_DESCENT_ONLY_UI && false
-    const val F_SCHEDULE_DESCENT_ONLY = true // WIP // TODO delete when :parser cleanup
-
-    // const val F_HTML_POI_NAME_UI = false
-    const val F_HTML_POI_NAME_UI = true // WIP // TODO delete when :parser cleanup
-    // const val F_HTML_POI_NAME = F_HTML_POI_NAME_UI && false
-    const val F_HTML_POI_NAME = true // WIP // TODO delete when :parser cleanup
-
     // const val F_SCHEDULE_INFINITE = false
     const val F_SCHEDULE_INFINITE = true // WIP
 
