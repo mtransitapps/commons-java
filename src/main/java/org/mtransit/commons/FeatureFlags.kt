db2f6c2a213269971dfdae5e1113a4f98299ee3e
Mathieu Méa <mathieu.mea@gmail.com>
2023-02-08 19:39:53 -0500

    Revert "Feature flag > a11y producer (data sources) > OFF (temp fix)"
    This reverts commit 5fefe0a2c7e9a2a85f275ede1893d8f6523e193c.

--- src/main/java/org/mtransit/commons/FeatureFlags.kt
+++ src/main/java/org/mtransit/commons/FeatureFlags.kt
@@ -16,8 +16,8 @@ object FeatureFlags {
     // const val F_SCHEDULE_INFINITE = false
     const val F_SCHEDULE_INFINITE = true // WIP
 
-    const val F_ACCESSIBILITY_PRODUCER = false
-    // const val F_ACCESSIBILITY_PRODUCER = true // WIP // data sources
+    // const val F_ACCESSIBILITY_PRODUCER = false
+    const val F_ACCESSIBILITY_PRODUCER = true // WIP // data sources
     // const val F_ACCESSIBILITY_CONSUMER = false
     const val F_ACCESSIBILITY_CONSUMER = true // WIP // UI
 
