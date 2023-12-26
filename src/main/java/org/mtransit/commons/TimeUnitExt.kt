edce08bd27a7afa297f4646e086f132585dc4ff5
Mathieu Méa <mathieu.mea@gmail.com>
2023-12-26 14:53:56 -0500

    TimeUnit extensions

---
+++ src/main/java/org/mtransit/commons/TimeUnitExt.kt
@@ -0,0 +1,9 @@
+package org.mtransit.commons
+
+import java.util.concurrent.TimeUnit
+
+fun Long.secToMs() = TimeUnit.SECONDS.toMillis(this)
+
+fun Int.secToMs() = this.toLong().secToMs()
+
+fun Long.msToSec() = TimeUnit.MILLISECONDS.toSeconds(this)
