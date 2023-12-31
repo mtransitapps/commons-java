10e5d4eaede1cf6af5d80a839706c8cb00ec5b11
Mathieu Méa <mathieu.mea@gmail.com>
2023-12-31 15:57:45 -0500

    TimeUnit Ktx++

--- src/main/java/org/mtransit/commons/TimeUnitExt.kt
+++ src/main/java/org/mtransit/commons/TimeUnitExt.kt
@@ -7,3 +7,7 @@ fun Long.secToMs() = TimeUnit.SECONDS.toMillis(this)
 fun Int.secToMs() = this.toLong().secToMs()
 
 fun Long.msToSec() = TimeUnit.MILLISECONDS.toSeconds(this)
+
+fun Long.daysToMs() = TimeUnit.DAYS.toMillis(this)
+
+fun Int.daysToMs() = this.toLong().daysToMs()
