044531fce63bb2a11c31a402abca69aee5480115
Mathieu Méa <mathieu.mea@gmail.com>
2024-11-13 19:29:23 -0500

    cleanup

--- src/main/java/org/mtransit/commons/SourceUtils.kt
+++ src/main/java/org/mtransit/commons/SourceUtils.kt
@@ -7,7 +7,12 @@ object SourceUtils {
     private val LOG_TAG: String = SourceUtils::class.java.simpleName
 
     @JvmStatic
-    fun getSourceLabel(urlString: String?) = urlString?.let { getSourceLabel(URL(it)) }
+    fun getSourceLabel(urlString: String?) = urlString?.let {
+        getSourceLabel(
+            @Suppress("DEPRECATION") // since Java 20
+            URL(it)
+        )
+    }
 
     @JvmStatic
     fun getSourceLabel(url: URL?): String? {
