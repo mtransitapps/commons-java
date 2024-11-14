7e0f76a6e800b8d45a56a5dce82ac41b23c0e1d3
Mathieu Méa <mathieu.mea@gmail.com>
2024-11-13 21:16:06 -0500

    Java scratch

---
+++ src/main/java/org/mtransit/scratch/JavaScratch.java
@@ -0,0 +1,9 @@
+package org.mtransit.scratch;
+
+public class JavaScratch {
+
+	public static void main(String[] args) {
+		System.out.println("-------------------------------------------");
+		System.out.println("-------------------------------------------");
+	}
+}
