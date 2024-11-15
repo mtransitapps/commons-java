63f99ee3a5516e92663ee7fcc204176140cf817d
Mathieu Méa <mathieu.mea@gmail.com>
2024-11-14 20:09:15 -0500

    Source label > remove "api", "data", "gbfs", "gtfs", "opendata",

--- src/main/java/org/mtransit/commons/SourceUtils.kt
+++ src/main/java/org/mtransit/commons/SourceUtils.kt
@@ -6,6 +6,15 @@ object SourceUtils {
 
     private val LOG_TAG: String = SourceUtils::class.java.simpleName
 
+    private val BLACK_LIST = listOf(
+        "api",
+        "data",
+        "gbfs",
+        "gtfs",
+        "opendata",
+        "www",
+    )
+
     @JvmStatic
     fun getSourceLabel(urlString: String?) = urlString?.let {
         getSourceLabel(
@@ -17,7 +26,9 @@ object SourceUtils {
     @JvmStatic
     fun getSourceLabel(url: URL?): String? {
         try {
-            return url?.host?.removePrefix("www.")?.split('.')?.takeLast(3)?.joinToString(separator = ".")
+            return url?.host?.split('.')?.takeLast(3)
+                ?.dropWhile(minSize = 2) { BLACK_LIST.contains(it) } // drop 1st elements if in black list
+                ?.joinToString(separator = ".")
         } catch (e: Exception) {
             System.err.println("$LOG_TAG: Error while parsing source label from URL $url!")
             e.printStackTrace()
--- src/test/java/org/mtransit/commons/SourceUtilsTests.kt
+++ src/test/java/org/mtransit/commons/SourceUtilsTests.kt
@@ -31,11 +31,56 @@ class SourceUtilsTests {
     }
 
     @Test
-    fun `test label from URL - www_example_qc_ca`() {
+    fun `test label from URL - api_example_qc_ca`() {
         val urlString = "https://api.example.qc.ca"
 
         val result = SourceUtils.getSourceLabel(urlString)
 
         assertEquals("example.qc.ca", result)
+    }
+
+    @Test
+    fun `test label from URL - api_example_com`() {
+        val urlString = "https://api.example.com"
+
+        val result = SourceUtils.getSourceLabel(urlString)
+
+        assertEquals("example.com", result)
+    }
+
+    @Test
+    fun `test label from URL - opendata_example_qc_ca`() {
+        val urlString = "https://opendata.example.com"
+
+        val result = SourceUtils.getSourceLabel(urlString)
+
+        assertEquals("example.com", result)
+    }
+
+    @Test
+    fun `test label from URL - gtfs_example_qc_ca`() {
+        val urlString = "https://gtfs.example.com"
+
+        val result = SourceUtils.getSourceLabel(urlString)
+
+        assertEquals("example.com", result)
+    }
+
+    @Test
+    fun `test label from URL - gbfs_example_qc_ca`() {
+        val urlString = "https://gbfs.example.com"
+
+        val result = SourceUtils.getSourceLabel(urlString)
+
+        assertEquals("example.com", result)
+    }
+
+    @Test
+    fun `test label from URL - gbfs_test_gbfs_example_qc_ca`() {
+        val urlString = "https://api.gbfs.com"
+
+        val result = SourceUtils.getSourceLabel(urlString)
+
+        assertEquals("gbfs.com", result)
     }
 }
