00a80abb173402a2ddb837a5173dd3cc29f4a5ab
Mathieu Méa <mathieu.mea@gmail.com>
2024-12-04 17:50:49 -0500

    comments

--- src/main/java/org/mtransit/commons/gtfs/sql/AllSQL.kt
+++ src/main/java/org/mtransit/commons/gtfs/sql/AllSQL.kt
@@ -2,4 +2,6 @@ package org.mtransit.commons.gtfs.sql
 
 val ALL_SQL_TABLES: List<TableSQL> = listOf(
     AgencySQL, RouteSQL, StopSQL, CalendarDateSQL
+    // TODO trip & stop_times
+    // other can wait, those ones would unlock real-time trip updates & vehicle positions
 )
