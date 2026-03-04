587ef667f1aa24f5ad51a051cbfa9c9445e1a20a
Mathieu Méa <mathieu.mea@gmail.com>
2026-03-04 11:51:49 -0500

    Add "last trip stop"
    - https://github.com/mtransitapps/parser/pull/47

--- src/main/java/org/mtransit/commons/FeatureFlags.kt
+++ src/main/java/org/mtransit/commons/FeatureFlags.kt
@@ -65,5 +65,8 @@ object FeatureFlags {
     // const val F_USE_TRIP_IS_FOR_SERVICE_UPDATES = false
     const val F_USE_TRIP_IS_FOR_SERVICE_UPDATES = true // WIP
 
+    const val F_EXPORT_DIRECTION_STOP_LAST = false
+    // const val F_EXPORT_DIRECTION_STOP_LAST = true // WIP
+
     // @formatter:on
 }
