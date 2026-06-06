d272dd91137d13a912455f896c84856632a4eded
Mathieu Méa <mathieu.mea@gmail.com>
2026-06-05 21:26:44 -0400

    Strings cleaner > clean string from language in order of config

--- src/main/java/org/mtransit/commons/StringsCleaner.kt
+++ src/main/java/org/mtransit/commons/StringsCleaner.kt
@@ -150,24 +150,28 @@ object StringsCleaner {
             }
         }
         string = CleanUtils.cleanSlashes(string)
-        if (languages?.contains(Locale.ENGLISH) == true) {
-            string = CleanUtils.fixMcXCase(string)
-            string = CleanUtils.CLEAN_AT.matcher(string).replaceAll(CleanUtils.CLEAN_AT_REPLACEMENT)
-            if (short) {
-                string = CleanUtils.CLEAN_AND.matcher(string).replaceAll(CleanUtils.CLEAN_AND_REPLACEMENT)
-                string = CleanUtils.cleanStreetTypes(string)
-            }
-            string = CleanUtils.cleanNumbers(string)
-        }
-        if (languages?.contains(Locale.FRENCH) == true) {
-            if (short) {
-                string = CleanUtils.CLEAN_ET.matcher(string).replaceAll(CleanUtils.CLEAN_ET_REPLACEMENT)
-                string = CleanUtils.SAINT.matcher(string).replaceAll(CleanUtils.SAINT_REPLACEMENT)
-                string = CleanUtils.cleanStreetTypesFRCA(string)
-                string = CleanUtils.removePointsI(string) // BEFORE next regexes
-                string = CleanUtils.ALL_FACE_A_REGEX.replace(string, CleanUtils.ALL_FACE_A_REGEX_REPLACEMENT)
-                string = CleanUtils.ALL_ST_REGEX.replace(string, CleanUtils.ALL_ST_REGEX_REPLACEMENT)
-                string = CleanUtils.ALL_CHARS_REGEX.replace(string, CleanUtils.ALL_CHARS_REGEX_REPLACEMENT)
+        languages?.forEach { language ->
+            when (language) {
+                Locale.ENGLISH -> {
+                    string = CleanUtils.fixMcXCase(string)
+                    string = CleanUtils.CLEAN_AT.matcher(string).replaceAll(CleanUtils.CLEAN_AT_REPLACEMENT)
+                    if (short) {
+                        string = CleanUtils.CLEAN_AND.matcher(string).replaceAll(CleanUtils.CLEAN_AND_REPLACEMENT)
+                        string = CleanUtils.cleanStreetTypes(string)
+                    }
+                    string = CleanUtils.cleanNumbers(string)
+                }
+                Locale.FRENCH -> {
+                    if (short) {
+                        string = CleanUtils.CLEAN_ET.matcher(string).replaceAll(CleanUtils.CLEAN_ET_REPLACEMENT)
+                        string = CleanUtils.SAINT.matcher(string).replaceAll(CleanUtils.SAINT_REPLACEMENT)
+                        string = CleanUtils.cleanStreetTypesFRCA(string)
+                        string = CleanUtils.removePointsI(string) // BEFORE next regexes
+                        string = CleanUtils.ALL_FACE_A_REGEX.replace(string, CleanUtils.ALL_FACE_A_REGEX_REPLACEMENT)
+                        string = CleanUtils.ALL_ST_REGEX.replace(string, CleanUtils.ALL_ST_REGEX_REPLACEMENT)
+                        string = CleanUtils.ALL_CHARS_REGEX.replace(string, CleanUtils.ALL_CHARS_REGEX_REPLACEMENT)
+                    }
+                }
             }
         }
         languages?.forEach { language ->
