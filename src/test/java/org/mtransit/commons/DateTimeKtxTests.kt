e2dd16a6d8e1170f377d9d21ff0f6a9974c508b8
Mathieu Méa <mathieu.mea@gmail.com>
2026-07-28 15:09:24 -0400

    fix `assert` -> `assertEquals`

--- src/test/java/org/mtransit/commons/DateTimeKtxTests.kt
+++ src/test/java/org/mtransit/commons/DateTimeKtxTests.kt
@@ -2,6 +2,7 @@ package org.mtransit.commons
 
 import kotlinx.datetime.DatePeriod
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class DateTimeKtxTests {
 
@@ -9,27 +10,27 @@ class DateTimeKtxTests {
     fun test_DatePeriod_weeks() {
         DatePeriod(days = 0)
             .weeks.let { result ->
-                assert(result == 0)
+                assertEquals(0, result)
             }
         DatePeriod(days = 6)
             .weeks.let { result ->
-                assert(result == 0)
+                assertEquals(0, result)
             }
         DatePeriod(days = 7)
             .weeks.let { result ->
-                assert(result == 1)
+                assertEquals(1, result)
             }
         DatePeriod(days = 8)
             .weeks.let { result ->
-                assert(result == 0)
+                assertEquals(0, result)
             }
         DatePeriod(months = 1)
             .weeks.let { result ->
-                assert(result == 0)
+                assertEquals(0, result)
             }
         DatePeriod(months = 1, days = 7)
             .weeks.let { result ->
-                assert(result == 0)
+                assertEquals(0, result)
             }
     }
 }
